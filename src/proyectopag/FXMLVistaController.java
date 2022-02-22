a5b50e6f679a55ac7dd4fa0318438fb84f4356a4
saulvc28 <saul.vega.2001@gmail.com>
2022-02-22 12:34:20 -0500

    Primer avance

---
+++ src/proyectopag/FXMLVistaController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopag;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author VEGA CASTRO
+ */
+public class FXMLVistaController implements Initializable {
+
+    @FXML
+    private AnchorPane lblClear;
+    @FXML
+    private ImageView imgFondo;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Label label;
+    @FXML
+    private Label lblUsuario;
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private Label lblContraseña;
+    @FXML
+    private Label lblBienvenido;
+    @FXML
+    private Button btnClear;
+    @FXML
+    private PasswordField pswd;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+}
